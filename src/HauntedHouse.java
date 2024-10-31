5ebeedbbf56ae0fd3a7d8b564a1841b05343a88b
Layth-m <156365859+Layth-m@users.noreply.github.com>
2024-10-31 12:26:27 -0500

    Add files via upload

---
+++ src/HauntedHouse.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions.*;
+public class HauntedHouse {
+    private boolean ghostPresent;
+    private int candyCount;
+
+    public HauntedHouse() {
+        ghostPresent = true;
+        candyCount = 10;
+    }
+
+    public boolean isGhostPresent() {
+        return ghostPresent;
+    }
+
+    public void scareAwayGhost() {
+        ghostPresent = false;
+    }
+
+
+    public void refillCandyBowl(int amount) {
+        // no negative candy allowed
+        if(amount>=0) {
+            candyCount += amount;
+        }
+    }
+
+    public void trickOrTreat(int people){
+
+        // only deduct if people is less than candyCount
+        if(people<=candyCount) {
+            candyCount = candyCount - people;
+
+        }
+        else{
+
+            //more people than candy = candyCount = 0
+            candyCount = 0;
+        }
+    }
+
+    public int getCandyCount() {
+        return candyCount;
+    }
+
+    public String spookySound() {
+        return "Boo!";
+    }
+
+    public int RunningLow(){
+
+        // make sure  amount refills after candy count is 0
+        if(candyCount==0){
+            refillCandyBowl(50);
+        }
+        return candyCount;
+    }
+
+    public void Haunting() {
+        ghostPresent = true;
+    }
+
+
+    @Override
+    public String toString() {
+        String result = "The house ";
+
+        if(isGhostPresent()) {
+            result += "is haunted by a Ghost and ";
+        }
+        result += "has " + candyCount + " candy.";
+        return result;
+    }
+}
